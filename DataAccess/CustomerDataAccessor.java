780cd88aef49170fdbbd47e9bad70c4288a1f92b
Bilal Asif Mirza <bilal.asif.97@gmail.com>
2017-01-22 16:53:34 +0500

    DataAccess folder

---
+++ DataAccess/CustomerDataAccessor.java
@@ -0,0 +1,12 @@
+package DataAccess;
+
+import Models.Customer;
+import org.hibernate.mapping.Table;
+
+/**
+ * Created by BilalAM on 1/17/2017.
+ */
+public class CustomerDataAccessor extends DataAccessor<Customer> {
+
+
+}
---
+++ DataAccess/EmployeeDataAccess.java
@@ -0,0 +1,10 @@
+package DataAccess;
+
+import Models.Employee;
+
+/**
+ * Created by BilalAM on 1/18/2017.
+ */
+public class EmployeeDataAccess extends DataAccessor<Employee> {
+
+}
